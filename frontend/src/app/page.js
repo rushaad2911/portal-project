b4ef46bb1eb0aa36fac51066c88278b2377b7840
rushaad29 <m.rushaadq@gmail.com>
2025-05-30 16:53:40 +0530

    created backend apps

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,11 +1,19 @@
-"useCache";
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className="main-style">
-    <h1 style={{color:"black"}}>hi</h1>
+    <div className={styles["main-style"]}>
+      <div className={styles["image-wrapper"]}>
+        <Image
+          src="https://media.istockphoto.com/id/1305019435/vector/graduation-leaders.jpg?s=612x612&w=0&k=20&c=FjwkCitkMjz_dCFyQjcNWf0UtTsW_7jLNGlnm8999ps="
+          alt="Graduation Leaders"
+          fill
+          className={styles["home-img"]}
+          unoptimized
+        />
+        <div className={styles.overlay}></div>
+      </div>
     </div>
   );
 }
